1c9c69d012640dec5e0b4f65c66053fba130eb40
Thiago Pereira <thiagorock554@gmail.com>
2021-07-15 20:32:29 -0300

    Adicionando funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+function ehpar(num){
+    return (num%2 == 0);
+}
+
+const ispar = function(num){
+    return (num%2 == 0);
+}
+
+
+// let resultado = ehpar(2);
+resultado = ispar(2);
+console.log('é par ' , resultado);
